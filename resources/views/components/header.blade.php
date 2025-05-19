4c43e6257a24e48045979824dbd708c2ca05b5a4
royboot <rgrizell467@gmail.com>
2025-05-19 16:37:39 +0200

    Added components to layout

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,7 +1,10 @@
-<header>
-    <nav>
-        <ol>
+<header class="h-[4vh] flex-container bg-amber-200">
+    <nav class="flex-container w-full">
+        <ol class="justify-around flex w-full flex-row">
             <li><a>Hello</a></li>
+            <li><a>Hi</a></li>
+            <li><a>Hi</a></li>
+            <li><a>Hi</a></li>
         </ol>
     </nav>
 </header>
--- resources/views/components/layouts/layout.blade.php
+++ resources/views/components/layouts/layout.blade.php
@@ -1,16 +1,10 @@
 <html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-    <link rel="stylesheet" href="{{ asset("css/style.css") }}">
-</head>
-
-<body>
-@include("components.header")
-<main>
-    @yield('content')
-</main>
-
-</body>
+    <x-head/>
+    <body class="h-[100vh] absolute top-0 w-[100vw]">
+        <x-header/>
+        <main class="h-[95vh] flex-container">
+            @yield('content')
+        </main>
+        <x-footer/>
+    </body>
 </html>
