181879fc027c7c1c9cb7ca7f7fcb90d909096a4c
royboot <rgrizell467@gmail.com>
2025-05-22 12:29:59 +0200

    changed routes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DateController;
 use Livewire\Volt\Volt;
 
 Route::get('/', function () {
     return view('home');
 })->name('home');
-
+Route::get("/About", [DateController::class, "about"])->name('about');
+Route::get("/Contact", function () {
+    return view('Contact');
+})->name('contact');
+Route::get("Projects", function () {
+    return view('Projects');
+})->name('projects');
 
 require __DIR__.'/auth.php';
