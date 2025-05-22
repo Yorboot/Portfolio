e345fa9dff953c1f6a34b493c516cdaaca74d774
royboot <rgrizell467@gmail.com>
2025-05-22 12:31:59 +0200

    added routes to header.blade.php

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,8 +1,8 @@
-<header class="h-[4vh] flex-container ">
+<header class="h-[4vh] flex-container shadow-[5px_10px_10px_0px_rgba(59,_130,_246,_0.5)] ">
     <nav class="flex-container w-full">
         <ol class="justify-around flex w-full flex-row">
-            <li><a>Home</a></li>
-            <li><a>About me</a></li>
+            <li><a href="{{route("home")}}">Home</a></li>
+            <li><a href="{{route("about")}}">About me</a></li>
             <li><a>Projects</a></li>
             <li><a>Contact</a></li>
         </ol>
