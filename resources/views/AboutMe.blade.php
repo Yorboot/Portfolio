d77a00e8dcfe0e59c6b5a580b712589f6808ffba
royboot <rgrizell467@gmail.com>
2025-05-22 12:32:56 +0200

    Added new views

---
+++ resources/views/AboutMe.blade.php
@@ -0,0 +1,6 @@
+@extends("components.layouts.CenteredLayout")
+@section('content')
+    <div class="flex-container  justify-center">
+        <h1>Hi I am Roy a {{$date}}</h1>
+    </div>
+@endsection
---
+++ resources/views/Projects.blade.php
@@ -0,0 +1,7 @@
+@extends("components.layouts.CenteredLayout")
+@section('content')
+    <div class="flex-container  justify-center">
+        <h1 class="text-4xl">Welcome to my portfolio site take a look around</h1>
+    </div>
+@endsection
+    
